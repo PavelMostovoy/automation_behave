2bad0679f9c13e8090a34db3bbb040d3a992c2ff
svetall2002 <svetall2002@gmail.com>
2020-02-17 17:31:18 +0200

    added cheking input value

--- script_1.py
+++ script_1.py
@@ -1,10 +1,23 @@
 from script_2 import *
+import time
 
-v = input("value V:")
 
-s = input("value S :")
+def input_through_checker(inputdescription):
+    while (True):
+        try:
+            entered_value = int(input(f"Enter a {inputdescription} "))
+            break
+        except ValueError:
+            print("Invalid number")
+    return int(entered_value)
+
+v = input_through_checker("value numeric V:")
+
+s = input_through_checker("velue numeric S")
+
 
 for f in range(100):
     volume_ = volume(v, s, f)
     # time.sleep(1)
-    print("for f  = {}  value = {}".format(f, volume_))
+    print("for f  = {}  value = {}".format(f,volume_))
+
